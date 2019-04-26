b2fa13229e01be096f7178646dabec729e6fcb94
khalidmeister <alghani_irfan@yahoo.com>
2019-04-26 22:23:38 +0700

    Menambahkan segi Back End di bagian artikel (CRUD menggunakan PHP + MySQL)

---
+++ safeneighborhood/lib/pages/add_article.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AddArticle extends StatelessWidget {
+  TextEditingController _judulController = TextEditingController();
+  TextEditingController _subJudulController = TextEditingController();
+  TextEditingController _isiController = TextEditingController();
+
+  void addData(){
+    var url = "http://192.168.0.113/tabel_sn/add_data.php";
+    http.post(url, body:
+      {
+        "judul": _judulController.text,
+        "sub_judul": _subJudulController.text,
+        "deskripsi": _isiController.text,
+      }
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Article"),
+      ),
+      body: new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new ListView(
+          children: <Widget>[
+            new Column(
+              children: <Widget>[
+                new TextField(
+                  controller: _judulController,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(16.0),
+                    labelText: "Judul dari artikel",
+                  ),
+                ),
+                new SizedBox(height: 10.0),
+                new TextField(
+                  controller: _subJudulController,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(16.0),
+                    labelText: "Sub-Judul dari artikel",
+                  ),
+                ),
+                new SizedBox(height: 10.0),
+                new TextField(
+                  controller: _isiController,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(16.0),
+                    labelText: "Isi dari artikel",
+                  ),
+                  maxLines: 5,
+                ),
+                new SizedBox(height: 10.0),
+                new SizedBox(
+                  width: double.infinity,
+                  height: 40.0,
+                  child: new RaisedButton(
+                    onPressed: () {
+                      addData();
+                      Navigator.pop(context);
+                      /*print(_isiController.text);
+                      print(_judulController.text);
+                      print(_subJudulController.text);*/
+                    },
+                    color: Colors.blueAccent,
+                    child: new Text("Submit", style: TextStyle(fontSize: 12.0, color: Colors.white),),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- safeneighborhood/lib/pages/article_section.dart
+++ safeneighborhood/lib/pages/article_section.dart
@@ -1,26 +1,74 @@
 import 'package:flutter/material.dart';
+import 'add_article.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 class ArticleSection extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return new Scaffold(
       appBar: AppBar(
-        title: new Text("Search"),
+        title: new Text("Article"),
       ),
       body: new Center(
-        child: new Text("Ini Article Page"),
+        child: ArticleList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blueAccent,
+        child: new Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute( builder: (context) => AddArticle())
+          );
+        },
       ),
     );
   }
 
-  Widget _buildArticles(){
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (BuildContext context, int i) {
-        if(i.isOdd){
-          new SizedBox(height: 1.0,);
-        }
+  
+} 
+
+class ArticleList extends StatefulWidget {
+  @override
+  _ArticleListState createState() => _ArticleListState();
+}
+
+class _ArticleListState extends State<ArticleList> {
+  Future<List> getData() async{
+    final response = await http.get("http://192.168.0.113/tabel_sn/get_data.php");
+    return json.decode(response.body);
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return new FutureBuilder<List>(
+      future: getData(),
+      builder: (context, snapshot) {
+        if(snapshot.hasError) print(snapshot.error);
+
+        return snapshot.hasData ? new ItemList(list: snapshot.data) : new CircularProgressIndicator();
       }
     );
   }
-} 
+}
+
+class ItemList extends StatelessWidget {
+  List list;
+  ItemList({this.list});
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (context, i) {
+        return ListTile(
+          leading: Icon(Icons.widgets),
+          title: new Text(list[i]["judul"]),
+          subtitle: new Text(list[i]["sub_judul"]),
+        );
+      },
+    );
+  }
+}
